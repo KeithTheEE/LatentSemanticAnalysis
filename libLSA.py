bd558c29ae1df76c3570651425790e52f0701ff6
CrakeNotSnowman <kmurrayis@gmail.com>
2015-10-20 04:52:09 -0500

    LSA Algorithm which uses NMF

--- libLSA.py
+++ libLSA.py
@@ -4,16 +4,72 @@ import os
 import sys
 import numpy
 import math 
-import sklearn
+from sklearn.decomposition import ProjectedGradientNMF
+import matplotlib.pyplot as plt
 
 # Keith Murray
 
+def matrix_factorization(R, P, Q, K, steps=5000, alpha=0.0002, beta=0.02):
+    """
+# Created by Albert Au Yeung (2010)
+#
+# An implementation of matrix factorization
+@INPUT:
+    R     : a matrix to be factorized, dimension N x M
+    P     : an initial matrix of dimension N x K
+    Q     : an initial matrix of dimension M x K
+    K     : the number of latent features
+    steps : the maximum number of steps to perform the optimisation
+    alpha : the learning rate
+    beta  : the regularization parameter
+@OUTPUT:
+    the final matrices P and Q
+accessed on Oct 14 2015
+http://www.quuxlabs.com/blog/2010/09/matrix-factorization-a-simple-tutorial-and-implementation-in-python/
+    """
+    def progress():
+	print("X"),
+    Q = Q.T
+    lastStep = 0
+    #print("["),
+    es = []
+    for step in xrange(steps):
+        for i in xrange(len(R)):
+            for j in xrange(len(R[i])):
+                if R[i][j] > 0:
+                    eij = R[i][j] - numpy.dot(P[i,:],Q[:,j])
+                    for k in xrange(K):
+                        P[i][k] = P[i][k] + alpha * (2 * eij * Q[k][j] - beta * P[i][k])
+                        Q[k][j] = Q[k][j] + alpha * (2 * eij * P[i][k] - beta * Q[k][j])
+        eR = numpy.dot(P,Q)
+        e = 0
+        for i in xrange(len(R)):
+            for j in xrange(len(R[i])):
+                if R[i][j] > 0:
+                    e = e + pow(R[i][j] - numpy.dot(P[i,:],Q[:,j]), 2)
+                    for k in xrange(K):
+                        e = e + (beta/2) * ( pow(P[i][k],2) + pow(Q[k][j],2) )
+        if e < 0.001:
+            break
+	es.append(e)
+	#print e
+	#if int(step/float(steps)*10) > lastStep:
+	    #lastStep = int(step/float(steps)*10)
+	    #progress()
+    #if lastStep > 10:
+	#for i in range(lastStep, 10):
+	    #progress()
+    #print("]")
+    
+    return P, Q.T, es
+
 class termDocMatrix(object):
-   def __init__(self, newThing, docs=""):
+    def __init__(self, newThing="", docs=""):
 	# Get term-document matrix
 	# transormation/modified weighting of term-doc matrix 
 	# dimensionality reduction
 	# clustering of documents in reduced space
+	self.wcThreshold = 2
 	self.mD = {}
 	self.tdm = []
 	self.tdmraw = []
@@ -23,9 +79,14 @@ class termDocMatrix(object):
 	self.terms = []
 	self.docCount = 0
         self.idfweight = False
-	self.add(newThing, docs)
+	if newThing != "":
+	    self.add(newThing, docs)
+	self.P = []
+	self.Q = []
+	self.er = []
+	self.idfs = []
 	return
-   def add(self, newThing, docs=""):
+    def add(self, newThing, docs=""):
 	def mergeDict(self, newD, docs=""):
 	    # Grab doc count so far
 	    docIndex = self.docCount
@@ -33,7 +94,7 @@ class termDocMatrix(object):
 	    if docs == "":
 		docs = docIndex
 	    self.docs.append(docs)
-	    # Add doc size (useful wiht tdmraw)
+	    # Add doc size (useful with tdmraw)
 	    tdWeight = float(sum(newD.values()))
 	    if tdWeight == 0:
 		tdWeight = 1.
@@ -45,15 +106,15 @@ class termDocMatrix(object):
 		    if len(self.mD[key]) < self.docCount:
 			for i in range(len(self.mD[key]), self.docCount):
 			    self.mD[key].append(0.)
-		    self.mD[key].append(newD[key]/tdWeight)
+		    self.mD[key].append(newD[key]/float(tdWeight))
 		else:
 		    if self.docCount > 0:
 			self.mD[key] = [0.]
 		        for i in range(1, self.docCount):
 			    self.mD[key].append(0.)
-			self.mD[key].append(newD[key]/tdWeight)
+			self.mD[key].append(newD[key]/float(tdWeight))
 		    else:
-			self.mD[key] =  [newD[key]/tdWeight]
+			self.mD[key] =  [newD[key]/float(tdWeight)]
 	    self.docCount += 1
 	    return
 	    
@@ -100,7 +161,7 @@ class termDocMatrix(object):
 		    newD[strList[i].strip()] = 1
 	    mergeDict(self, newD)
 	return
-   def weight_idf(self):
+    def weight_idf(self):
 	# Now we're weighting it, booyea
 	# There are two forms of weights applied here
 	#   td: each term divided by the total terms from that doc
@@ -127,7 +188,8 @@ class termDocMatrix(object):
 		    if self.mD[key][i] == 0:
 			idf = True
 			break
-	    if idf == True:
+	    # CURRENTLY AN ERROR DUE TO TD WEIGHTING EARLIER
+	    if (len(filter(None, self.mD[key])) >= self.wcThreshold) and idf == True:
 		self.terms.append(key)
 		self.tdm.append(self.mD[key])
 	
@@ -142,14 +204,90 @@ class termDocMatrix(object):
 	    #print ni, self.docCount
 	    idfValue = math.log(self.docCount/ni)
 	    self.tdm[i] = self.tdm[i] * idfValue
+	    self.idfs.append(idfValue)
 	    
 	return
-		    
 
+    def svd(self):
+	return
+
+    def nmf(self, k):
+	
+	nmf = ProjectedGradientNMF(n_components=k, max_iter=200)
+	print len(self.tdm), len(self.tdm[0])
+	P = nmf.fit_transform(self.tdm)
+	Q = nmf.components_.T
+	print len(P), len(P[0]),  len(Q), len(Q[0])
+	#print nmf.components_
+	#print nmf
+	# R     : a matrix to be factorized, dimension N x M
+	# P     : an initial matrix of dimension N x K
+	# Q     : an initial matrix of dimension M x K
+	#P = numpy.random.random(size=(len(self.tdm), k))
+	#Q = numpy.random.random(size=(len(self.tdm[0]), k))
+	#P, Q, es = matrix_factorization(self.tdm, P, Q, k, steps=5000, alpha=0.0002, beta=0.02)
+	self.P = P
+	self.Q = Q
+	self.er = nmf.reconstruction_err_
+	print "\tError: ", self.er
+	return P, Q
+ 
+    def saveParts(self, location=""):
+	# Check Location
+	if location != "":
+	    if location[-1] != "/":
+		location = str(location) + "/"
+	    if not os.path.exists(location): os.makedirs(location)
+	# Save Terms
+	termSet = open(str(location) + "terms.lst", 'w')
+	for i in range(len(self.terms)):
+	    termSet.write(str(self.terms[i]) +"\n")
+	termSet.close()
+	# Save Docs
+	docSet = open(str(location) + "docs.lsd", 'w')
+	for i in range(len(self.docs)):
+	    docSet.write(str(self.docs[i]) +"\n")
+	docSet.close()
+	tdmR = open(str(location) + "tdmRaw.lsm", 'w')
+	for i in range(len(self.tdmraw)):
+	    for j in range(len(self.tdmraw[i])):
+		tdmR.write(str(self.tdmraw[i][j]) + "\t")
+	    tdmR.write("\n")
+	tdmR.close()
+	p = open(str(location) + "Pmatrix.lsp", 'w')
+	for i in range(len(self.P)):
+	    for j in range(len(self.P[i])):
+		p.write(str(self.P[i][j]) + "\t")
+	    p.write("\n")
+	p.close()
+	q = open(str(location) + "Qmatrix.lsq", 'w')
+	for i in range(len(self.Q)):
+	    for j in range(len(self.Q[i])):
+		q.write(str(self.Q[i][j]) + "\t")
+	    q.write("\n")
+	q.close()
+	idfs = open(str(location) + "idf.lsi", 'w')
+	for i in range(len(self.idfs)):
+	    idfs.write(str(self.idfs[i]) + "\n")
+	idfs.close()
+
+	'''
+	plt.plot(self.er)
+	plt.xlabel("Itteration")
+	plt.ylabel("Error")
+	plt.title("Error During NMF: Final = " + str(self.er[-1]))
+	plt.savefig(str(location) + "Error.pdf", bbox_inches='tight')
+	plt.savefig(str(location) + "Error.png", bbox_inches='tight')
+	plt.close()
+	'''
+	# AMI DIST MAT
+	return
+	    
+	
 	    
 
 
-   def __repr__(self):
+    def __repr__(self):
 	if self.idfweight == False:
 	    a = "\t"
 	    for i in range(len(self.docs)):
--- mainLSA.py
+++ mainLSA.py
@@ -3,6 +3,8 @@
 import os
 import sys
 import numpy
+import gzip,cPickle
+import math
 
 import libLSA
 
@@ -10,27 +12,167 @@ import libLSA
 
    
 
-def getTweetsToDict(infile):
-    pass
+
+def save(D, filename):
+    file = gzip.open(filename+'.gzip', 'w')
+    cPickle.dump(D, file)
+    file.close()
+
+def load(filename):
+    file = gzip.open(filename+'.gzip')
+    #KEITH EDIT
+    #file = gzip.open(filename)
+    D=cPickle.load(file)
+    file.close()
+    return D
     
 
+def getTweetsToDict(lsa):
+    #dSet = open("/home/keith/Documents/filesForProgramming/Twitter/lsaDB/earthquake/tweetProof.txt", 'r')
+    #clnDS = open("/home/keith/Documents/filesForProgramming/Twitter/lsaDB/earthquake/tweetProofC.txt", 'w')
+    dSet = open("toyData.txt", 'r')
+    clnDS = open("toyDataC.txt", 'w')
+    for line in dSet:
+	if line not in ["\n", ""]:
+	    lsa.add(line.strip().lower())
+	    clnDS.write(line.lower())
+    dSet.close()
+    clnDS.close()
+    return lsa
+    
+    
+def euclidean(a, b) :
+    i = 0
+    x = 0
+    y = 0
+    difference = 0
+    euclideanDistance = 0
+    sumEuc = 0
+    for i in range(len(a)) :
+        x = float(a[i])
+        y = float(b[i])
+        
+        difference = (x - y)
+        sumEuc += difference*difference
+    euclideanDistance = math.sqrt(sumEuc)
+    '''
+    if ( euclideanDistance != 0) :
+        euclideanDistance = 1 / euclideanDistance
+    else :
+        euclideanDistance = 1
+    '''
+    return euclideanDistance
+def Correlation(a, b):
+
+    i = 0
+    j = 0
+    meanA = 0
+    meanB = 0
+    for i in range(len(a)) :
+        meanA = meanA + float(a[i])
+    for j in range(len(b)) :
+        meanB = meanB + float(b[j])
+
+    meanA = float(meanA)/len(a)
+    meanB = float(meanB)/len(b)
+    
+    k = 0
+    l = 0
+    A = 0
+    Asqrt = 0
+    B = 0
+    Bsqrt = 0
+    dotProduct = 0
+    denom = 0
+    distance = 0
+
+    formatDistance = 0
+
+    for k in range(len(a)) :
+        A = float(a[k]) #- meanA
+        B = float(b[k]) #- meanB
+
+        dotProduct = dotProduct + (A - meanA)*(B - meanB)
+        
+        Asqrt = Asqrt + ((A - meanA) * (A - meanA))
+        Bsqrt = Bsqrt + ((B - meanB) * (B - meanB)) 
+
+   # print Asqrt
+   # print Bsqrt
+    Asqrt = math.sqrt(Asqrt)
+    Bsqrt = math.sqrt(Bsqrt)
+   # print Asqrt
+   # print Bsqrt
+    
+    
+    denom = Asqrt * Bsqrt
+    if denom == 0:
+	denom = .000000001
+    distance = float(dotProduct)/denom
+
+    correlationDistance = 1 - distance 
+        
+    return correlationDistance
 
 
+def nspectDocs(mat, filename='nSpectDM.dm'):
+    print len(mat)
+    dm = [[0.0 for y in range(len(mat))] for x in range(len(mat))]
+    dmM = 0
+    for i in range(len(mat)):
+	for j in range(len(mat)):
+	    if i != j:
+		dm[i][j] = euclidean(mat[i], mat[j])
+		if dm[i][j] > dmM:
+		    dmM = dm[i][j]
+    dm = numpy.array(dm)
+    dm = dm/float(dmM)
+    #dmf = open('/home/keith/Documents/filesForProgramming/Twitter/lsaDB/earthquake/nSpectDM.dm', 'w')
+    dmf = open(filename, 'w')
+    dmf.write(str(len(mat)) + "\n")
+    for i in range(len(mat)):
+	dmf.write("Seq" + str(i) + '\t')
+	for j in range(len(mat)):
+	    dmf.write(str.format("{0:.10f}", dm[i][j]))
+	    if j != len(mat):
+		dmf.write('\t')
+	dmf.write('\n')
+    dmf.close()
 
+def cluster(count, lsa):
+    from sklearn.cluster import KMeans
+    km = KMeans(n_clusters=count, init='random', n_init=1, verbose=1)
+    km.fit(lsa.P)
+    tData = open("toyDatP.dl", 'w')
+    for i in range(len(km.labels_)):
+	tData.write(str(km.labels_[i]) + " 0 4\n")
+    tData.close()
 
 def main():
     # Get term-document matrix
     # transormation/modified weighting of term-doc matrix 
     # dimensionality reduction
     # clustering of documents in reduced space
-
-    lsa = libLSA.termDocMatrix("a b b c c c")
-    lsa.add("a a a b b b b d d d d d")
-    lsa.add("transormation/modified weighting of a term-doc matrix")
-    print "PYTHON"
-    print lsa
+    lsa = libLSA.termDocMatrix()
+    lsa = getTweetsToDict(lsa)
+    print "\tAdded to Dict\n\tidf now"
     lsa.weight_idf()
-    print lsa
+    #print lsa.terms, len(lsa.mD.keys())
+    print "\tnfm"
+    #save(lsa, "/home/keith/Documents/filesForProgramming/Twitter/lsaDB/tornado/dataStructSave")
+    #lsa = load("/home/keith/Documents/filesForProgramming/Twitter/lsaDB/tornado/dataStructSave")
+    #print "\tloaded"
+    P, Q = lsa.nmf(20)
+    print "\tsaving"
+    #save(lsa, "/home/keith/Documents/filesForProgramming/Twitter/lsaDB/earthquake/dataStructSave")
+    save(lsa, "dataStructSave")
+    #lsa.saveParts("/home/keith/Documents/filesForProgramming/Twitter/lsaDB/earthquake/")
+    lsa.saveParts()
+    #lsa = load("quicks")
+    print "\tnSpect DM Loading"
+    nspectDocs(lsa.Q)
+    nspectDocs(lsa.P, filename='nSpectDMP.dm')
+    cluster(15, lsa)
     '''
     temp = open("lsa.xlsx", 'w')
     temp.write(lsa.__repr__())
